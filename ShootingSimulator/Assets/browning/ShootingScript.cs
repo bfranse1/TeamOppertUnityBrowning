4a567678845410e2279e3e2409773c1db6c65b36
Adit Permadi <perm0002@hz.nl>
2018-09-18 14:30:01 +0200

    ShootingSimulator added

---
+++ ShootingSimulator/Assets/browning/ShootingScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetKeyDown("e"))
+		{
+			print("space key was pressed");
+		}
+	}
+}
+
