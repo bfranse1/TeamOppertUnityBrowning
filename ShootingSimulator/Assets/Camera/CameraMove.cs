9f12f9ba0bbedfd61ccfd4d13a203cf07d606ec3
Adit Permadi <perm0002@hz.nl>
2018-09-24 08:47:19 +0200

    Game added #2

---
+++ ShootingSimulator/Assets/Camera/CameraMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public Vector3 center;
+
+	Vector3 v = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * Time.deltaTime * 30);
+		transform.Rotate(Vector3.right * Time.deltaTime * 8, Space.World);
+	}
+}
