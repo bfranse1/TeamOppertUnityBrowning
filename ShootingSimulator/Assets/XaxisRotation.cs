9191c4d4e9a2c10834b31f23c20cc1bae8595aec
Lars van de Vrede <larsvandevrede@hotmail.com>
2018-11-09 13:05:41 +0100

    Added rotation scripts

---
+++ ShootingSimulator/Assets/XaxisRotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XaxisRotation : MonoBehaviour {
+
+    public browning bro;
+    public float horizontalSpeed = 10.0f;
+    public float minY = -50.0f;
+    public float maxY = 50.0f;
+    Vector3 euler;
+    public int startPosition;
+
+    // Use this for initialization
+    void Start () {
+        euler.y = startPosition;
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = euler;
+
+        if (bro.camera2.activeSelf == true)
+            {
+                float h = horizontalSpeed * Input.GetAxis("Mouse X") * Time.deltaTime;
+
+                euler.y += h;
+
+            if (euler.y >= maxY)
+                    euler.y = maxY;
+                if (euler.y <= minY)
+                    euler.y = minY;
+
+        }
+    }
+}
---
+++ ShootingSimulator/Assets/gunmovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunmovement : MonoBehaviour
+{
+
+    public browning bro;
+    public float verticalSpeed = 300.0f;
+    public float min = -50.0f;
+    public float max = 50.0f;
+    Vector3 euler;
+    public int startPosition = -180;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        euler.x = startPosition;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.localEulerAngles = euler;
+
+        if (bro.camera2.activeSelf == true)
+	    {
+	        float v = verticalSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+	        euler.x += v;
+
+	        if (euler.x >= max)
+	            euler.x = max;
+	        if (euler.x <= min)
+	            euler.x = min;
+        }
+ 
+    }
+}
